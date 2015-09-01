fdc0a7642721eff2a7c978354c1761687afd23de
Josenaldo <josenaldols@gmail.com>
2015-09-01 19:13:03 -0300

    Ajustes

--- webServices/usuariosWebService.php
+++ webServices/usuariosWebService.php
@@ -5,36 +5,39 @@ session_start();
 extract($_REQUEST);
 extract($_SESSION);
 
-//INSERIR USUÁRIO
-if (isset($_POST["tipo"])) {
-	if ($_POST["tipo"] == "incluir") {
-
+//CONSULTA USUÁRIO
+	if (isset($_GET["incluirUsuario"])) {			
+		
 		//Classe de Usuário
-		$usuario = new Usuario();
+		$usuario = new Usuario();	
 
-		//Atributos da classe Usuário/Valores
-		$usuario -> setIdPerfil($idPerfil);
-		$usuario -> setNome($nome);
-		$usuario -> setRazaoSocial($razaoSocial);
-		$usuario -> setFantasia($fantasia);
-		$usuario -> setCpfCnpj($cpfcnpj);
-		$usuario -> setEmail($email);
-		$usuario -> setTelefone1($telefone1);
-		$usuario -> setTelefone2($telefone2);
-		$usuario -> setLogradouro($logradouro);
-		$usuario -> setBairro($bairro);
-		$usuario -> setCep($cep);
-
-		$usuario -> setLogin($login);
-		$usuario -> setSenha($senha);
-
+		//Atributos da classe Usuário/Valores 
+		$usuario->setIdPerfil($idPerfil); 
+		$usuario->setNome($nome); 
+		$usuario->setRazaoSocial($razaoSocial); 
+		$usuario->setFantasia($fantasia); 
+		$usuario->setCpfCnpj($cpfcnpj); 
+		$usuario->setEmail($email); 
+		$usuario->setTelefone1($telefone1); 
+		$usuario->setTelefone2($telefone2); 
+		$usuario->setLogradouro($logradouro); 
+		$usuario->setBairro($bairro); 
+		$usuario->setCep($cep); 
+		$usuario->setObjCidade($codCidade); 
+				
+		$usuario->setLogin($login); 
+		$usuario->setSenha($senha); 				
+		
 		//echo('1');
 
-		if (UsuarioSql::adicionar($usuario)) {
-			$resultado[] = array('oka' => 'oks', );
-		}
-
-		echo json_encode($resultado);
+		if (UsuarioSql::adicionar($usuario)){
+			$resultado[] = array(				
+				'oka'	=>  'oks',						
+			);			
+		}	
+		
+		echo(json_encode($resultado ));			
+	
 	}
 }
 
