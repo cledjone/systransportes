5c3be79f27f969f94fcc996be0294020d5fb5564
marcelloteofilo <marcello.312@hotmail.com>
2015-09-05 11:24:48 -0300

    Mudanças Template/Css

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../css/paginaTemplate.css">
 
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"> </script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"> </script>
@@ -18,45 +19,6 @@
         <script type="text/javascript" src="../js/jquery-ui.js"> </script>
         <script type="text/javascript" src="../js/scriptsUsuarios.js"> </script>
         <script type="text/javascript" src="../js/scriptsCidades.js"> </script>
-
-
- <style type="text/css">
-            body{ background-image: url("http://noticias.prestumseguros.com.br/wp-content/uploads/2013/12/seguro-caminhao.jpg");
-            } 
-            .transparent-style{ background-color: #000000; opacity: .9; }
-            .navbar,
-            .dropdown-menu{ background:rgba(255,255,255,0.75); border-bottom: 3px solid transparent; } 
-            .nav>li>a,
-            .dropdown-menu>li>a:focus, 
-            .dropdown-menu>li>a:hover, 
-            .dropdown-menu>li>a,
-            .dropdown-menu>li{ border-bottom: 3px solid transparent; } 
-            .nav>li>a:focus,
-            .nav>li>a:hover,.nav .open>a, .nav .open>a:focus, .nav .open>a:hover, 
-            .dropdown-menu>li>a:focus,
-            .dropdown-menu>li>a:hover{ border-bottom: 3px solid transparent; background:rgba(245,255,255,0.10); } 
-            .navbar a, 
-            .dropdown-menu>li>a, 
-            .dropdown-menu>li>a:focus, 
-            .dropdown-menu>li>a:hover{ color: #071B39; } 
-            .dropdown-menu{ -webkit-box-shadow: none; box-shadow:none;} 
-            .nav li:hover:nth-child(8n+1), 
-            .nav li.active:nth-child(8n+1){ border-bottom: #C4E17F 3px solid; } 
-            .nav li:hover:nth-child(8n+2), 
-            .nav li.active:nth-child(8n+2){border-bottom: #F7FDCA 3px solid; } 
-            .nav li:hover:nth-child(8n+3), 
-            .nav li .active:nth-child(8n+3){ border-bottom: #FECF71 3px solid; } 
-            .nav li:hover:nth-child(8n+4),
-            .nav li.active:nth-child(8n+4){ border-bottom: #F0776C 3px solid; } 
-            .nav li:hover:nth-child(8n+5), 
-            .nav li.active:nth-child(8n+5){ border-bottom: #DB9DBE 3px solid; } 
-            .nav li:hover:nth-child(8n+6), 
-            .nav li.active:nth-child(8n+6){ border-bottom: #C49CDE 3px solid; } 
-            .nav li:hover:nth-child(8n+7), 
-            .nav li.active:nth-child(8n+7){ border-bottom: #669AE1 3px solid; } 
-            .nav li:hover:nth-child(8n+8),
-            .nav li.active:nth-child(8n+8){ border-bottom: #62C2E4 3px solid; }
-        </style>
 
     </head>
     
