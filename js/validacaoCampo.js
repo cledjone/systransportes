c848f9a6860a7d32745a4b62565142905adca4d1
Thiagoadsunibratec <thiago.ads.unibratec01@gmail.com>
2015-09-09 10:09:24 -0300

    Terminei as validações do cadastro de pessoa fisica.
    Foi concluido as validações do cadastro de pessoa fisica, agora estou
    indo para juridia..

--- js/validacaoCampo.js
+++ js/validacaoCampo.js
@@ -125,3 +125,26 @@ function verificacaoSenha(){
 	}
 }
 //--------------------VALIDANDO SENHA------------------------------
+
+//----------------------VALIDAÇÃO DE LETRAS E NUMEROS--------------------------
+function validar(dom,tipo){
+	switch(tipo){
+		case'num':var regex=/[A-Za-z]/g;break;
+		case'text':var regex=/\d/g;break;
+	}
+	dom.value=dom.value.replace(regex,'');
+}
+//----------------------VALIDAÇÃO DE LETRAS--------------------------
+
+
+
+function telefoneMascara(telefone){ 
+   if(telefone.value.length == 0)
+     telefone.value = '(' + telefone.value; //quando começamos a digitar, o script irá inserir um parênteses no começo do campo.
+   if(telefone.value.length == 3)
+      telefone.value = telefone.value + ') '; //quando o campo já tiver 3 caracteres (um parênteses e 2 números) o script irá inserir mais um parênteses, fechando assim o código de área.
+ 
+ if(telefone.value.length == 9)
+     telefone.value = telefone.value + '-'; //quando o campo já tiver 8 caracteres, o script irá inserir um tracinho, para melhor visualização do telefone.
+  
+}
